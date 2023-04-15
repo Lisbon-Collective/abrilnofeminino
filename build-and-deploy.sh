da7977961c6c1a9057fc73703d4249bbb5aed431
Bruno Amaral <mail@brunoamaral.eu>
2023-04-15 12:06:30 +0100

    update credentials

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -2,4 +2,4 @@
 ./hugo -d public
 #rsync -azh --delete public/ bruno@abade-faria:/var/www/brunoamaral.eu/ | bar -s $(du -s public | awk '{print $1}')
 
-rsync -azh --info=progress2 --stats --delete public/ alfama:/var/www/abrilnofeminino.pt/
+rsync -azh --no-times --info=progress2 --stats --delete public/ fernando@lisboa:/var/www/abrilnofeminino.pt/
